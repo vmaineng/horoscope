4c59e6064c94ff9bddf7d1fed1a5143e08db18dd
Mai Vang <vmaineng@gmail.com>
2026-09-18 19:11:25 -0700

    added in bakcend

--- app/hooks/useHoroscope.ts
+++ app/hooks/useHoroscope.ts
@@ -11,7 +11,7 @@ export default function useHoroscope() {
     setName(info.name);
 
     try {
-      const res = await fetch("/api/horoscope", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/horoscope`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ dob: info.dob }),
